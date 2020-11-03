a8e8493fccd460b5c9711972a17f87cf5017d327
GogoBarretto <73837571+GogoBarretto@users.noreply.github.com>
2020-11-03 05:20:04 +0300

    Add files via upload

---
+++ RTE_Task_03/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTE_Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isAccessGranted = false;
+            string password = "password";
+            for(int i = 0; i < 3;i++)
+            {
+                Console.Write("Введите пароль: ");
+                string password_check = Console.ReadLine();
+                if (password == password_check)
+                {
+                    isAccessGranted = true;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Пароль введён не правильно, у вас осталось {2 - i} попыток для ввода");
+                    Console.WriteLine();
+                }
+            }
+            if (isAccessGranted == true)
+            {
+                Console.WriteLine("Вы успешно вошли в систему");
+            }
+            else
+            {
+                Console.WriteLine("Вы исчерпали все попытки входа, доступ не получен.");
+            }
+            Console.ReadLine();
+        }
+    }
+}

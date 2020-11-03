4a27025cde4192dfe7f28d23875a36ab832cc7a2
GogoBarretto <73837571+GogoBarretto@users.noreply.github.com>
2020-11-03 05:09:19 +0300

    Add files via upload

---
+++ RTE_Task_01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TNS_Task_01
+{
+
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            int price = 10;
+            Console.Write("Введите количество вашего золота: ");
+            int gold = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Обменный курс золота на кристаллы : { price } золота = 1 кристалл. ");
+            Console.Write("Сколько кристаллов вы хотите купить?");
+            int crystals = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                int rest = gold - crystals * price;
+                int[] check = new int[gold + 1];
+                check[rest] = 1;
+                Console.WriteLine($"Операция успешна. У вас осталось {rest} золота вы получили {crystals} кристалл(ов).");
+            }
+            catch
+            {
+                Console.WriteLine($"Сделка невозможна, у вас {gold} золота и 0 кристаллов.");
+            }
+            Console.ReadKey();
+        }
+    }
+
+
+}
---
+++ RTE_Task_02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTE_Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any words you want (Word 'exit' ends the program): ");
+            string exit = "";
+            do
+            {
+                exit = Console.ReadLine();
+            } while (exit != "exit");
+        }
+    }
+}

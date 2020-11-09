432f7f003158dcea5bb2903f392839ea4223b35b
GogoBarretto <73837571+GogoBarretto@users.noreply.github.com>
2020-11-09 05:30:26 +0300

    Update Program.cs

--- RTE_Task_01/Program.cs
+++ RTE_Task_01/Program.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TNS_Task_01
+namespace RTE_Task_01
 {
 
     class Program
@@ -13,24 +13,20 @@ namespace TNS_Task_01
         
         static void Main(string[] args)
         {
+            bool purchase;
             int price = 10;
             Console.Write("Введите количество вашего золота: ");
             int gold = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Обменный курс золота на кристаллы : { price } золота = 1 кристалл. ");
-            Console.Write("Сколько кристаллов вы хотите купить?");
+            Console.Write("Сколько кристаллов вы хотите купить? : ");
             int crystals = Convert.ToInt32(Console.ReadLine());
 
-            try
-            {
-                int rest = gold - crystals * price;
-                int[] check = new int[gold + 1];
-                check[rest] = 1;
-                Console.WriteLine($"Операция успешна. У вас осталось {rest} золота вы получили {crystals} кристалл(ов).");
-            }
-            catch
-            {
-                Console.WriteLine($"Сделка невозможна, у вас {gold} золота и 0 кристаллов.");
-            }
+
+            purchase = gold >= price * crystals;
+            crystals *= Convert.ToInt32(purchase);
+            gold -= price * crystals;
+            Console.WriteLine($"У вас осталось {gold} золота вы получили {crystals} кристалл(ов).");
+
             Console.ReadKey();
         }
     }
